2d1709d6f78f37ac0161dbe0192a987915c4a5da
Arkadiusz Chodór <arek@chodor.dev>
2022-11-04 11:38:21 +0100

    Feature: Add errors handling

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -11,6 +11,7 @@ export default function SearchInput() {
   return (
     <input
       className="w-full rounded-full border border-zinc-700 bg-zinc-900/50 px-5 py-2 text-zinc-300 placeholder-zinc-600 shadow-sm focus:border-rose-500 focus:outline-none focus:ring-1 focus:ring-rose-500 lg:w-96"
+      name="search"
       placeholder="Search"
       defaultValue={searchParams.get('q') || ''}
       onChange={e => search(e.target.value)}
--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,24 @@ import './index.css';
 import Root from './routes/root';
 import Videos, { loader as videosLoader } from './routes/videos';
 import VideoId, { loader as videoLoader } from './routes/videoId';
+import ErrorMessage from './components/ErrorMessage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorMessage />,
     children: [
       {
         path: '/videos',
         element: <Videos />,
         loader: videosLoader,
+        errorElement: <ErrorMessage />,
         children: [
           {
             path: '/videos/:id',
             element: <VideoId />,
+            errorElement: <ErrorMessage />,
             loader: videoLoader
           }
         ]
